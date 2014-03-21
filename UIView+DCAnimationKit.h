f52ff34075dff27b8eabc85a8a7a511f64d3781c
Dalton Cherry <daltoniam@gmail.com>
2014-03-21 15:20:40 -0700

    initial animations

---
+++ UIView+DCAnimationKit.h
@@ -0,0 +1,237 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  UIView+AnimationKit.h
+//
+//  Created by Dalton Cherry on 3/20/14.
+//
+//////////////////////////////////////////////////////////////////////////////////////
+
+#import <UIKit/UIKit.h>
+
+typedef void (^DCAnimationFinished)(void);
+
+@interface UIView (DCAnimationKit)
+
+///-------------------------------
+/// @name coordinate manipulation
+///-------------------------------
+
+/**
+ set the x location you want the view to set
+ @param x is the x coordinate you want to set.
+ @param time is the duration of the animation.
+ @param finished is called when the animation finishes
+*/
+-(void)setX:(CGFloat)x duration:(NSTimeInterval)time finished:(DCAnimationFinished)finished;
+
+/**
+ set the x location you want the view to move to.
+ @param x is the x coordinate you want to move to.
+ @param finished is called when the animation finishes
+*/
+-(void)setX:(CGFloat)x finished:(DCAnimationFinished)finished;
+
+/**
+ move the x location (e.g. the x of the view's frame is 200 and x is 50, 
+ then the x coordinate of the view's frame will be 250).
+ @param x is the offset of the x coordinate you want to move by.
+ @param time is the duration of the animation.
+ @param finished is called when the animation finishes
+ */
+-(void)moveX:(CGFloat)x duration:(NSTimeInterval)time finished:(DCAnimationFinished)finished;
+
+/**
+ move the x location (e.g. the x of the view's frame is 200 and x is 50,
+ then the x coordinate of the view's frame will be 250).
+ @param x is the offset of the x coordinate you want to move by.
+ @param finished is called when the animation finishes
+ */
+-(void)moveX:(CGFloat)x finished:(DCAnimationFinished)finished;
+
+/**
+ set the y location you want the view to set
+ @param y is the x coordinate you want to set.
+ @param time is the duration of the animation.
+ @param finished is called when the animation finishes
+ */
+-(void)setY:(CGFloat)y duration:(NSTimeInterval)time finished:(DCAnimationFinished)finished;
+
+/**
+ set the y location you want the view to set
+ @param y is the y coordinate you want to set.
+ @param finished is called when the animation finishes
+ */
+-(void)setY:(CGFloat)y finished:(DCAnimationFinished)finished;
+
+/**
+ move the y location (e.g. the y of the view's frame is 200 and y is 50,
+ then the y coordinate of the view's frame will be 250).
+ @param y is the offset of the y coordinate you want to move by.
+ @param time is the duration of the animation.
+ @param finished is called when the animation finishes
+ */
+-(void)moveY:(CGFloat)y duration:(NSTimeInterval)time finished:(DCAnimationFinished)finished;
+
+/**
+ move the y location (e.g. the y of the view's frame is 200 and y is 50,
+ then the y coordinate of the view's frame will be 250).
+ @param y is the offset of the y coordinate you want to move by.
+ @param finished is called when the animation finishes
+ */
+-(void)moveY:(CGFloat)y finished:(DCAnimationFinished)finished;
+
+/**
+ set the point you want the view to set to.
+ @param point is the x and y coordinate you want to set.
+ @param time is the duration of the animation.
+ @param finished is called when the animation finishes
+ */
+-(void)setPoint:(CGPoint)point duration:(NSTimeInterval)time finished:(DCAnimationFinished)finished;
+
+/**
+ set the point you want the view to set to.
+ @param point is the x and y coordinate you want to set.
+ @param finished is called when the animation finishes
+ */
+-(void)setPoint:(CGPoint)point finished:(DCAnimationFinished)finished;
+
+/**
+ move the x and y location (e.g. x,y is 10,20 and the point is 30,30,
+ then the x and y coordinates of the view's frame will be 40,50).
+ @param point is the offset of the x,y coordinates you want to move by.
+ @param time is the duration of the animation.
+ @param finished is called when the animation finishes
+ */
+-(void)movePoint:(CGPoint)point duration:(NSTimeInterval)time finished:(DCAnimationFinished)finished;
+
+/**
+ move the x and y location (e.g. x,y is 10,20 and the point is 30,30,
+ then the x and y coordinates of the view's frame will be 40,50).
+ @param point is the offset of the x,y coordinates you want to move by.
+ @param finished is called when the animation finishes
+ */
+-(void)movePoint:(CGPoint)point finished:(DCAnimationFinished)finished;
+
+
+/**
+ set the degrees of rotation on the view
+ @param r is the degrees you want the view to be rotated by. This would be a value between 0 and 360.
+ @param time is the duration of the animation.
+ @param finished is called when the animation finishes
+ */
+-(void)setRotation:(CGFloat)r duration:(NSTimeInterval)time finished:(DCAnimationFinished)finished;
+
+/**
+ set the degrees of rotation on the view
+ @param r is the degrees you want the view to be rotated by. This would be a value between 0 and 360.
+ @param finished is called when the animation finishes
+ */
+-(void)setRotation:(CGFloat)r finished:(DCAnimationFinished)finished;
+
+/**
+ move the view by a degree of rotation
+ @param r is the degrees you want the view to be rotated by. This would be a value between 0 and 360.
+ @param time is the duration of the animation.
+ @param finished is called when the animation finishes
+ */
+-(void)moveRotation:(CGFloat)r duration:(NSTimeInterval)time finished:(DCAnimationFinished)finished;
+
+/**
+ move the view by a degree of rotation
+ @param r is the degrees you want the view to be rotated by. This would be a value between 0 and 360.
+ @param finished is called when the animation finishes
+ */
+-(void)moveRotation:(CGFloat)r finished:(DCAnimationFinished)finished;
+
+///-------------------------------
+/// @name Attention grabbers
+///-------------------------------
+
+/**
+ preform the bounce animation.
+ @param finished is called when the animation finishes
+ */
+-(void)bounce:(DCAnimationFinished)finished;
+
+/**
+ preform the pulse animation.
+ @param finished is called when the animation finishes
+ */
+-(void)pulse:(DCAnimationFinished)finished;
+
+/**
+ preform the shake animation.
+ @param finished is called when the animation finishes
+ */
+-(void)shake:(DCAnimationFinished)finished;
+
+/**
+ preform the swing animation.
+ @param finished is called when the animation finishes
+ */
+-(void)swing:(DCAnimationFinished)finished;
+
+/**
+ preform the tada animation.
+ @param finished is called when the animation finishes
+ */
+-(void)tada:(DCAnimationFinished)finished;
+
+
+///-------------------------------
+/// @name Intros
+///-------------------------------
+
+typedef NS_ENUM(NSInteger, DCAnimationDirection) {
+    DCAnimationDirectionTop,
+    DCAnimationDirectionBottom,
+    DCAnimationDirectionLeft,
+    DCAnimationDirectionRight
+};
+
+/**
+ The view enters from a direction and snaps into place.
+ @param view is the superview you want to snap into. It will add itself as a subview of view.
+ @param direction is the direction to enter from.
+ */
+-(void)snapIntoView:(UIView*)view direction:(DCAnimationDirection)direction;
+
+/**
+ The view enters from a direction and bounce into place.
+ @param view is the superview you want to bounce into. It will add itself as a subview of view.
+ @param direction is the direction to enter from.
+ */
+-(void)bounceIntoView:(UIView*)view direction:(DCAnimationDirection)direction;
+
+/**
+ The view expands into it's frame place.
+ @param view is the superview you want to expand into. It will add itself as a subview of view.
+ @param finished is called when the animation finishes
+ */
+-(void)expandIntoView:(UIView*)view finished:(DCAnimationFinished)finished;
+
+
+///-------------------------------
+/// @name Outros
+///-------------------------------
+
+/**
+ The view compress into it's frame place.
+ @param finished is called when the animation finishes
+ */
+-(void)compressIntoView:(DCAnimationFinished)finished;
+
+/**
+ The view compress into it's frame place.
+ @param finished is called when the animation finishes
+ */
+-(void)hinge:(DCAnimationFinished)finished;
+
+/**
+ The view will drop based on gravity.
+ @param finished is called when the animation finishes
+ */
+-(void)drop:(DCAnimationFinished)finished;
+
+
+@end
---
+++ UIView+DCAnimationKit.m
@@ -0,0 +1,437 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  UIView+AnimationKit.m
+//
+//  Created by Dalton Cherry on 3/20/14.
+//
+//////////////////////////////////////////////////////////////////////////////////////
+
+#import "UIView+DCAnimationKit.h"
+#import <objc/runtime.h>
+
+@interface UIImageView (DCAnimationKit)
+@property (readwrite, nonatomic, strong, setter = dc_setAnimator:)UIDynamicAnimator *dc_animator;
+@end
+
+@implementation UIView (DCAnimationKit)
+
+static NSTimeInterval DEFAULT_DURATION = 0.25;
+
+//////////////////////////////////////////////////////////////////////////////////////
+- (UIDynamicAnimator*)dc_animator {
+    
+    UIDynamicAnimator *animator = (UIDynamicAnimator*)objc_getAssociatedObject(self, @selector(dc_animator));
+    if(!animator)
+    {
+        animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.superview];
+        [self dc_setAnimator:animator];
+    }
+    return (UIDynamicAnimator*)objc_getAssociatedObject(self, @selector(dc_animator));
+}
+//////////////////////////////////////////////////////////////////////////////////////
+- (void)dc_setAnimator:(UIDynamicAnimator*)animator {
+    objc_setAssociatedObject(self, @selector(dc_animator), animator, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+//////////////////////////////////////////////////////////////////////////////////////
+CGFloat degreesToRadians(CGFloat degrees)
+{
+    return degrees * M_PI / 180;
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)setDirection:(DCAnimationDirection)direction
+{
+    //these need to be more accurate
+    CGRect frame = self.frame;
+    if(direction == DCAnimationDirectionBottom)
+        frame.origin.y = self.window.frame.size.height;
+    else if(direction == DCAnimationDirectionTop)
+        frame.origin.y = -self.window.frame.size.height;
+    else if(direction == DCAnimationDirectionLeft)
+        frame.origin.x = -self.window.frame.size.width;
+    else
+        frame.origin.x = self.window.frame.size.width;
+    self.frame = frame;
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(CGVector)vectorDirection:(DCAnimationDirection)direction
+{
+    if(direction == DCAnimationDirectionBottom)
+        return CGVectorMake(0, -1);
+    else if(direction == DCAnimationDirectionTop)
+        return CGVectorMake(0, 1);
+    else if(direction == DCAnimationDirectionLeft)
+        return CGVectorMake(1, 0);
+    else
+        return CGVectorMake(-1, 0);
+}
+//////////////////////////////////////////////////////////////////////////////////////
+
+#pragma general movements
+
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)setX:(CGFloat)x finished:(DCAnimationFinished)finished
+{
+    [self setX:x duration:DEFAULT_DURATION finished:finished];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)setX:(CGFloat)x duration:(NSTimeInterval)time finished:(DCAnimationFinished)finished
+{
+    [UIView animateWithDuration:time animations:^{
+        CGRect frame = self.frame;
+        frame.origin.x = x;
+        self.frame = frame;
+    } completion:^(BOOL f){
+        if(finished)
+            finished();
+    }];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)moveX:(CGFloat)x duration:(NSTimeInterval)time finished:(DCAnimationFinished)finished
+{
+    [self setX:(self.frame.origin.x+x) duration:time finished:finished];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)moveX:(CGFloat)x finished:(DCAnimationFinished)finished
+{
+    [self moveX:x duration:DEFAULT_DURATION finished:finished];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)setY:(CGFloat)y duration:(NSTimeInterval)time finished:(DCAnimationFinished)finished
+{
+    [UIView animateWithDuration:time animations:^{
+        CGRect frame = self.frame;
+        frame.origin.y = y;
+        self.frame = frame;
+    }completion:^(BOOL f){
+        if(finished)
+            finished();
+    }];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)setY:(CGFloat)y finished:(DCAnimationFinished)finished
+{
+    [self setY:y duration:DEFAULT_DURATION finished:finished];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)moveY:(CGFloat)y duration:(NSTimeInterval)time finished:(DCAnimationFinished)finished
+{
+    [self setY:(self.frame.origin.y+y) duration:time finished:finished];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)moveY:(CGFloat)y finished:(DCAnimationFinished)finished
+{
+    [self moveY:y duration:DEFAULT_DURATION finished:finished];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)setPoint:(CGPoint)point duration:(NSTimeInterval)time finished:(DCAnimationFinished)finished
+{
+    [UIView animateWithDuration:time animations:^{
+        CGRect frame = self.frame;
+        frame.origin = point;
+        self.frame = frame;
+    }completion:^(BOOL f){
+        if(finished)
+            finished();
+    }];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)setPoint:(CGPoint)point finished:(DCAnimationFinished)finished
+{
+    [self setPoint:point finished:finished];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)movePoint:(CGPoint)point duration:(NSTimeInterval)time finished:(DCAnimationFinished)finished
+{
+    [self setPoint:CGPointMake(self.frame.origin.x+point.x, self.frame.origin.y+point.y) duration:time finished:finished];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)movePoint:(CGPoint)point finished:(DCAnimationFinished)finished
+{
+    [self movePoint:point duration:DEFAULT_DURATION finished:finished];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)setRotation:(CGFloat)r duration:(NSTimeInterval)time finished:(DCAnimationFinished)finished
+{
+    [UIView animateWithDuration:time animations:^{
+        CGAffineTransform rotationTransform = CGAffineTransformIdentity;
+        self.transform = CGAffineTransformRotate(rotationTransform, degreesToRadians(r));
+    } completion:^(BOOL f){
+        if(finished)
+            finished();
+    }];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)setRotation:(CGFloat)r finished:(DCAnimationFinished)finished
+{
+    [self setRotation:r duration:DEFAULT_DURATION finished:finished];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)moveRotation:(CGFloat)r duration:(NSTimeInterval)time finished:(DCAnimationFinished)finished
+{
+    [UIView animateWithDuration:time animations:^{
+        CGAffineTransform rotationTransform = self.transform;
+        self.transform = CGAffineTransformRotate(rotationTransform, degreesToRadians(r));
+    } completion:^(BOOL f){
+        if(finished)
+            finished();
+    }];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)moveRotation:(CGFloat)r finished:(DCAnimationFinished)finished
+{
+    [self moveRotation:r duration:DEFAULT_DURATION finished:finished];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+
+#pragma attention grabbers
+
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)bounce:(DCAnimationFinished)finished
+{
+    [self moveY:-20 duration:0.25 finished:^{
+        [self moveY:20 duration:0.15 finished:^{
+            [self moveY:-5 duration:0.15 finished:^{
+                [self moveY:5 duration:0.05 finished:^{
+                    if(finished)
+                        finished();
+                }];
+            }];
+        }];
+    }];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)pulse:(DCAnimationFinished)finished
+{
+    [UIView animateWithDuration:0.5 animations:^{
+        self.transform = CGAffineTransformMakeScale(1.1, 1.1);
+    } completion:^(BOOL f){
+        [UIView animateWithDuration:0.5 delay:0.1 options:0 animations:^{
+            self.transform = CGAffineTransformMakeScale(1, 1);
+        } completion:^(BOOL f){
+            if(finished)
+                finished();
+        }];
+    }];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)shake:(DCAnimationFinished)finished
+{
+    float dist = 10;
+    [self moveX:-dist duration:0.15 finished:^{
+        [self moveX:dist*2 duration:0.15 finished:^{
+            [self moveX:-(dist*2) duration:0.15 finished:^{
+                [self moveX:dist duration:0.15 finished:^{
+                    if(finished)
+                        finished();
+                }];
+            }];
+        }];
+    }];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)swing:(DCAnimationFinished)finished
+{
+    float dist = 15;
+    float dur = 0.20;
+    __weak id weakSelf = self;
+    [weakSelf setRotation:dist duration:dur finished:^{
+        [weakSelf setRotation:-dist duration:dur finished:^{
+            [weakSelf setRotation:dist/2 duration:dur finished:^{
+                [weakSelf setRotation:-dist/2 duration:dur finished:^{
+                    [weakSelf setRotation:0 duration:dur finished:^{
+                        if(finished)
+                            finished();
+                    }];
+                }];
+            }];
+        }];
+    }];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)tada:(DCAnimationFinished)finished
+{
+    float dist = 3;
+    float dur = 0.12;
+    [UIView animateWithDuration:dur animations:^{
+        CGAffineTransform rotationTransform = CGAffineTransformMakeScale(0.95, 0.95);
+        rotationTransform = CGAffineTransformRotate(rotationTransform, degreesToRadians(dist));
+        self.transform = rotationTransform;
+    } completion:^(BOOL f){
+        [UIView animateWithDuration:dur animations:^{
+            CGAffineTransform rotationTransform = CGAffineTransformMakeScale(1.05, 1.05);
+            rotationTransform = CGAffineTransformRotate(rotationTransform, degreesToRadians(-dist));
+            self.transform = rotationTransform;
+        } completion:^(BOOL f){
+            
+            __weak id weakSelf = self;
+            [weakSelf moveRotation:dist*2 duration:dur finished:^{
+                [weakSelf moveRotation:-dist*2 duration:dur finished:^{
+                    [weakSelf moveRotation:dist*2 duration:dur finished:^{
+                        [weakSelf moveRotation:-dist*2 duration:dur finished:^{
+                            [UIView animateWithDuration:dur animations:^{
+                                CGAffineTransform rotationTransform = CGAffineTransformMakeScale(1, 1);
+                                rotationTransform = CGAffineTransformRotate(rotationTransform, degreesToRadians(0));
+                                self.transform = rotationTransform;
+                            } completion:^(BOOL f){
+                                if(finished)
+                                    finished();
+                            }];
+                        }];
+                    }];
+                }];
+            }];
+            
+        }];
+    }];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+
+#pragma intros
+
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)snapIntoView:(UIView*)view direction:(DCAnimationDirection)direction
+{
+    if(self.superview != view)
+        [self removeFromSuperview];
+    [view addSubview:self];
+    UIDynamicAnimator *animator = [self dc_animator];
+    [animator removeAllBehaviors];
+    
+    UISnapBehavior *snapBehaviour = [[UISnapBehavior alloc] initWithItem:self snapToPoint:self.center];
+    [self setDirection:direction];
+    snapBehaviour.damping = 0.75f;
+    [animator addBehavior:snapBehaviour];
+    
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)bounceIntoView:(UIView*)view direction:(DCAnimationDirection)direction
+{
+    if(self.superview != view)
+        [self removeFromSuperview];
+    [view addSubview:self];
+    UIDynamicAnimator *animator = [self dc_animator];
+    [animator removeAllBehaviors];
+    
+    UIAttachmentBehavior *behavior = [[UIAttachmentBehavior alloc] initWithItem:self attachedToAnchor:self.center];
+    [self setDirection:direction];
+    behavior.length = 0;
+    behavior.damping = 0.55;
+    behavior.frequency = 1.0;
+    [animator addBehavior:behavior];
+    
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)expandIntoView:(UIView *)view finished:(DCAnimationFinished)finished
+{
+    if(self.superview != view)
+        [self removeFromSuperview];
+    [view addSubview:self];
+    
+    self.transform = CGAffineTransformMakeScale(0, 0);
+    self.hidden = NO;
+    [UIView animateWithDuration:0.3 animations:^{
+        self.transform = CGAffineTransformMakeScale(1, 1);
+    } completion:^(BOOL f){
+        if(finished)
+            finished();
+    }];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+
+#pragma outros
+
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)compressIntoView:(DCAnimationFinished)finished
+{
+    [UIView animateWithDuration:0.3 animations:^{
+        self.transform = CGAffineTransformMakeScale(0, 0);
+    } completion:^(BOOL f){
+        self.hidden = YES;
+        if(finished)
+            finished();
+    }];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)hinge:(DCAnimationFinished)finished
+{
+    CGPoint point = CGPointMake(self.frame.origin.x, self.frame.origin.y);
+    self.layer.anchorPoint = CGPointMake(0, 0);
+    self.center = point;
+    __weak UIView *weakSelf = self;
+    float dur = 0.5;
+    [weakSelf setRotation:80 duration:dur finished:^{
+        [weakSelf setRotation:70 duration:dur finished:^{
+            [weakSelf setRotation:80 duration:dur finished:^{
+                [weakSelf setRotation:70 duration:dur finished:^{
+                    [self moveY:weakSelf.window.frame.size.height duration:dur finished:^{
+                        [self removeFromSuperview];
+                        if(finished)
+                            finished();
+                        [weakSelf setRotation:0 finished:NULL];
+                    }];
+                }];
+            }];
+        }];
+    }];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+-(void)drop:(DCAnimationFinished)finished
+{
+    UIDynamicAnimator *animator = [self dc_animator];
+    [animator removeAllBehaviors];
+    
+    UIGravityBehavior *gravityBehaviour = [[UIGravityBehavior alloc] initWithItems:@[self]];
+    gravityBehaviour.gravityDirection = CGVectorMake(0.0f, 10.0f);
+    [animator addBehavior:gravityBehaviour];
+    
+    UIDynamicItemBehavior *itemBehaviour = [[UIDynamicItemBehavior alloc] initWithItems:@[self]];
+    [itemBehaviour addAngularVelocity:-M_PI_2 forItem:self];
+    [animator addBehavior:itemBehaviour];
+    
+    [UIView animateWithDuration:0.5 animations:^{
+        self.alpha = 0;
+    } completion:^(BOOL f){
+        [self removeFromSuperview];
+        if(finished)
+            finished();
+    }];
+}
+//////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////
+//still not please with the animation of this, so taking out for now.
+/*-(void)bounceIntoView:(UIView*)view direction:(DCAnimationDirection)direction
+{
+    if(self.superview != view)
+        [self removeFromSuperview];
+    [view addSubview:self];
+    UIDynamicAnimator *animator = [self dc_animator];
+    [animator removeAllBehaviors];
+    [self setDirection:direction];
+    
+    __weak id weakSelf = self;
+    [self setX:0 duration:0.5 finished:^{
+        
+        UICollisionBehavior* collisionBehavior = [[UICollisionBehavior alloc] initWithItems:@[weakSelf]];
+        collisionBehavior.translatesReferenceBoundsIntoBoundary = YES;
+        [animator addBehavior:collisionBehavior];
+        
+        UIDynamicItemBehavior *elasticityBehavior = [[UIDynamicItemBehavior alloc] initWithItems:@[weakSelf]];
+        elasticityBehavior.elasticity = 0.7f;
+        [animator addBehavior:elasticityBehavior];
+        
+        UIGravityBehavior* gravityBehavior = [[UIGravityBehavior alloc] initWithItems:@[weakSelf]];
+        gravityBehavior.gravityDirection = [weakSelf vectorDirection:direction];
+        [animator addBehavior:gravityBehavior];
+        
+//        UIPushBehavior * push = [[UIPushBehavior alloc] initWithItems:@[weakSelf] mode:UIPushBehaviorModeInstantaneous];
+//        push.pushDirection = [weakSelf vectorDirection:direction];
+//        push.magnitude = 1;
+//        [animator addBehavior:push];
+//        push.active = YES;
+    }];
+    
+}*/
+//////////////////////////////////////////////////////////////////////////////////////
+
+@end
